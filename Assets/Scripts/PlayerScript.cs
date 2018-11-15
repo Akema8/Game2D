01732d348d73947ccb1bdd0fc262059b71ad1149
maggieD <rl-c@yandex.ru>
2018-11-15 03:23:14 +0300

    init

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(15, 15);
+
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+            rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+
+        //стрельба
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                weapon.Attack(false); //false так как игрок не враг
+            }
+        }
+		
+	}
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        bool damagePlayer = false;
+
+        EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+        if (enemy != null)
+        {
+            HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.Damage(enemyHealth.hp);
+
+                damagePlayer = true;
+            }
+        }
+
+        if (damagePlayer)
+        {
+            HealthScript playerHealth = GetComponent<HealthScript>();
+            if (playerHealth != null)
+                playerHealth.Damage(1);
+        }
+    }
+   
+}
---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+	public Transform shotPrefab;
+
+    public float shotingRate = 0.25f; //время перезарядки в сек
+
+    private float shotCooldown; //перезарядка
+
+
+    void Start()
+    {
+        shotCooldown = 0f;
+    }
+
+    void Update()
+    {
+        if (shotCooldown > 0)
+        {
+            shotCooldown -= Time.deltaTime;
+        }
+    }
+
+    public void Attack (bool isEnemy)
+    {
+        if (CanAttack)
+        {
+            shotCooldown = shotingRate;
+
+            var shotTransform = Instantiate(shotPrefab) as Transform;
+
+            shotTransform.position = transform.position;
+
+            ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+            if (shot != null)
+            {
+                shot.isEnemyShoot = isEnemy;
+            }
+
+            MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+            if (move != null)
+            {
+                move.direction = this.transform.right;
+            }
+
+        }
+    }
+
+    public bool CanAttack
+    {
+        get
+        {
+            return shotCooldown <= 0f;
+        }
+    }
+}
